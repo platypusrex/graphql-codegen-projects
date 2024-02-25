8fc6b7640b1d6cc0683ed7c53d11aed76fa7d4df
Frank Cooke <frankcooke79@gmail.com>
2024-02-25 10:26:41 -0500

    fix: typings issue with cjs require

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from './createProjectConfig';
-export * from './types';
--- src/types.ts
+++ src/types.ts
@@ -14,3 +14,8 @@ export type CodegenProject<TProject extends string> = IGraphQLProjects['projects
 export type ProjectsCodegenConfig<TProject extends string> = IGraphQLProjects & {
   projects: CodegenProject<TProject>;
 };
+
+export declare const createProjectConfig: <TProject extends string>(
+  config: CodegenProject<TProject>
+) => ProjectsCodegenConfig<TProject>;
+
